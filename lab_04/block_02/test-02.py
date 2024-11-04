280efb6cfe538a3cd3fbd18b8510a46fdeb544cf
DieG02 <diegob0505@gmail.com>
2024-11-04 21:49:18 +0100

    lab_04 block_02 completed

--- lab_04/block_02/test-02.py
+++ lab_04/block_02/test-02.py
@@ -1,8 +1,21 @@
-## TEMPLATE ##
+## DIAGNOSTIC IMAGING ##
+# NOTE: Fun fact, this was the most mathematical exercise 'till now
+import math
 
 print("\n")
-print("## TEMPLATE ##")
+print("## DIAGNOSTIC IMAGING ##")
 
-print("<------- Replace_me ------->")
+def get_diagnostic():
+  # Original Formula: 𝐴 = 𝐴_0 * e^(-𝜆t)  -> 𝜆 = ln (2) / T_(1/2)
+  
+  # Calculate relative amount of radiation: `A / A_0`
+  ln_2 = math.log(2) # If there is only one single parameter is, it uses `ln`
+  media_life = 6 # T_(1/2)
+  period_of_time = 24 # in hours
+  for t in range(period_of_time + 1):
+    lambda_val = ln_2 / media_life
+    amount = math.e ** (-lambda_val * t)
+    print(f"{'Hour ' + str(t) + ':':<12}{round(amount, 2)}")
 
+get_diagnostic()
 print("\n")
--- lab_04/block_02/test-03.py
+++ lab_04/block_02/test-03.py
@@ -1,8 +1,25 @@
-## TEMPLATE ##
+## VERTICAL SHOT ##
 
 print("\n")
-print("## TEMPLATE ##")
+print("## VERTICAL SHOT ##")
 
-print("<------- Replace_me ------->")
+print("")
+
+def vertical_shot():
+  DELTA_T = 0.01
+  G = 9.81
+  v_0 = int(input("v_0: ")) # 100
+  s_0 = 0
+  v = v_0
+  s = 0
+  
+  print(f"{round(s, 2):^10} | {round(v, 2):^10} | {round(DELTA_T, 2):^10}")
+  while v >= -v_0:
+    v = v_0 - G * DELTA_T
+    s = s_0 + v_0 * DELTA_T
+    DELTA_T += 0.01
+    print(f"{round(s, 2):^10} | {round(v, 2):^10} | {round(DELTA_T, 2):^10}")
+
+vertical_shot()
 
 print("\n")
